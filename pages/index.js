9075550297bd35819082a2fc0b46e8993f5288d1
jona199608 <subjona@gmail.com>
2021-11-03 15:34:07 -0300

    agregue

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          subi sorete{' '}
+          subi soretesssss{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
