186e60160eca86bf3727e4c5a68a627281ca283e
jonathan <87816454+jona199608@users.noreply.github.com>
2022-10-28 11:47:27 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create verga App</title>
+        <title>Create App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
